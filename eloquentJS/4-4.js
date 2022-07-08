ddc7ff57737c3931cf95227bec379021fd35cc5c
kavan4 <kavan.pandya@hotmail.com>
2022-07-08 11:27:53 -0400

    added comments on 4-4 for additional understanding

--- eloquentJS/4-4.js
+++ eloquentJS/4-4.js
@@ -9,6 +9,9 @@ function deepEqual(a, b) {
     if (keysA.length != keysB.length) return false;
   
     for (let key of keysA) {
+        //for object comparision, it is better to use obj[key]. here the value of key is dynamically replaced.
+        //obj.key looks for the string 'key' in obj. does not dynamically replace. same as obj['key'].
+        //dot notation is typically used for ease of use. [] brackets is better..?
       if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
     }
   
